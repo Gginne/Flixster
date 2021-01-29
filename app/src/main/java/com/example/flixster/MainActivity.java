e3be4c0bc7e1847d168cc15c58cb98f14603b7e1
Gginne <23042760+Gginne@users.noreply.github.com>
2021-01-28 21:26:26 -0500

    Added API connection and created Movie class

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String API_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+    public List<Movie> movies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(API_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                 JSONObject jsonObject = json.jsonObject;
+                 try{
+                     JSONArray res = jsonObject.getJSONArray("results");
+                     movies = Movie.fromJSONArray(res);
+                 } catch(JSONException e){
+                     Log.e(TAG, "Hit JSON Exception", e);
+                 }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
     }
 }
